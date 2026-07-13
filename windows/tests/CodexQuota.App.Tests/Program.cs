518cf87ce65d75691b480d62c1b721c2a5547107
MrPPFruit <49444331+MrPPFruit@users.noreply.github.com>
2026-07-14 02:13:52 +0800

    fix: import IO for Windows UI captures

--- windows/tests/CodexQuota.App.Tests/Program.cs
+++ windows/tests/CodexQuota.App.Tests/Program.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Globalization;
+using System.IO;
 
 var tests = new (string Name, Action Body)[]
 {
